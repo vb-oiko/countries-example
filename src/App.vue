b08419758b20a36ce50171633eb85a28f63bd58c
Vasyl Boyko <vboiko0@gmail.com>
2020-06-11 14:08:47 +0300

    add: filter countries by region

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,32 @@
     <v-app-bar app color="primary" dark>
       REST Countries API app example
     </v-app-bar>
-
     <v-content>
-      <country-card
-        v-for="country in countries"
-        :key="country.alpha2Code"
-        :country="country"
-        :translations="defaulTranslations"
-      />
+      <v-row>
+        <v-col cols="3">
+          <v-card>
+            <v-card-text>
+              <v-radio-group v-model="selectedRegion" mandatory>
+                <v-radio
+                  v-for="region in regions"
+                  :key="region"
+                  :label="region"
+                  :value="region"
+                >
+                </v-radio>
+              </v-radio-group>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="9">
+          <country-card
+            v-for="country in filteredCountries"
+            :key="country.alpha2Code"
+            :country="country"
+            :translations="defaulTranslations"
+          />
+        </v-col>
+      </v-row>
     </v-content>
   </v-app>
 </template>
@@ -20,6 +38,9 @@ import Vue from "vue";
 import CountriesService from "./services/countries-service";
 import { Country } from "@/types";
 import CountryCard from "@/components/CountryCard.vue";
+
+const EMPTY_REGION = "Not Applicable";
+const ALL_REGIONS = "All";
 
 export default Vue.extend({
   name: "App",
@@ -31,8 +52,30 @@ export default Vue.extend({
   data() {
     return {
       countries: [] as Country[],
-      defaulTranslations: ["de", "es", "it"]
+      defaulTranslations: ["de", "es", "it"],
+      selectedRegion: ALL_REGIONS
     };
+  },
+
+  computed: {
+    regions(): string[] {
+      const regions = [
+        ...new Set(this.countries.map(country => country.region))
+      ].map(region => (region === "" ? EMPTY_REGION : region));
+
+      return [...regions, ALL_REGIONS];
+    },
+
+    filteredCountries(): Country[] {
+      if (this.selectedRegion === ALL_REGIONS) {
+        return this.countries;
+      }
+
+      const filterRegion =
+        this.selectedRegion === EMPTY_REGION ? "" : this.selectedRegion;
+
+      return this.countries.filter(({ region }) => region === filterRegion);
+    }
   },
 
   async mounted() {
